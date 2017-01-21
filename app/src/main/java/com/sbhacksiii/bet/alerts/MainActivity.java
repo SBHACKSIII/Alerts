7c143a2cb005c2838234b51d0cac0271e746bcdd
Tanner Summers <Tannersummers@gmail.com>
2017-01-21 00:40:24 -0800

    - Updated

--- app/src/main/java/com/sbhacksiii/bet/alerts/MainActivity.java
+++ app/src/main/java/com/sbhacksiii/bet/alerts/MainActivity.java
@@ -1,11 +1,13 @@
-package com.example.eduardo.sbhacksiii;
+package com.sbhacksiii.bet.alerts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.sbhacksiii.bet.alerts.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
         mMapView = (MapView) findViewById(R.id.mapView);
         ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
         mMapView.setMap(map);
+
+
+        Log.i("Test","Test");
     }
 }
